4cfd0a07da841af7bff23258eae780c000a344c1
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-04-22 14:46:59 -0500

    added first name last name fields to register form

--- SimpleToDo/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SimpleToDo/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -50,6 +50,12 @@ namespace SimpleToDo.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
