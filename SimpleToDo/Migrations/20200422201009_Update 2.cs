381f5ded98be00a1d54c2d811c0c1065fdc2a85f
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-04-22 15:17:33 -0500

    bug with registration fixed

---
+++ SimpleToDo/Migrations/20200422201009_Update 2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+
+namespace SimpleToDo.Migrations
+{
+    public partial class Update2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "4a2509c7-95ee-4836-9eca-a66922c2dc48", "AQAAAAEAACcQAAAAEM7agNJzm8ZU5bmGGiJFxf0UkHKAUIs/xAycMOFJgpl65vwb4sBql8bN6TtbskccwA==" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "3477ef5e-bf0e-49bf-87e4-ca56b97d93b3", "AQAAAAEAACcQAAAAEOIcS9r/ch4fBqrXMsbnbl9eg1yGveIcZoGmAgtiv0Uhyp+I7ePR8gimXdBkrJg2Kg==" });
+        }
+    }
+}
---
+++ SimpleToDo/Models/ViewModels/TodoItemStatusViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.Models.ViewModels
+{
+    public class TodoItemStatusViewModel
+    {
+        public int TodoItemId { get; set; }
+
+        [Display(Name = "Title")]
+        [Required]
+        public string Title { get; set; }
+
+
+        [Display(Name = "Status")]
+        [Required]
+        public int TodoStatusId { get; set; }
+
+        [Display(Name = "Status")]
+        [Required]
+        public string Status { get; set; }
+        public List<SelectListItem> StatusOptions { get; set; }
+    }
+}
