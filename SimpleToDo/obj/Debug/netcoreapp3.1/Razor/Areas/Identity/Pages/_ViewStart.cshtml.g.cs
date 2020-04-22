bac49fed9bce4f366a8ac54075e48cb6fb4ba229
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-04-22 14:01:44 -0500

    scaffolded the login and register - user can now register and login

--- SimpleToDo/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ SimpleToDo/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,36 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Users\Mac Gibbons\source\repos\SimpleToDo\SimpleToDo\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\Mac Gibbons\source\repos\SimpleToDo\SimpleToDo\Areas\Identity\Pages\_ViewImports.cshtml"
+using SimpleToDo.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\Mac Gibbons\source\repos\SimpleToDo\SimpleToDo\Areas\Identity\Pages\_ViewImports.cshtml"
+using SimpleToDo.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 5 "C:\Users\Mac Gibbons\source\repos\SimpleToDo\SimpleToDo\Areas\Identity\Pages\_ViewImports.cshtml"
+using SimpleToDo.Models;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"13b82ef97968a56a4de533f67cf3e21d662f41aa", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7b38163094b4c7fb43d00ac1918338f38fa6394a", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
